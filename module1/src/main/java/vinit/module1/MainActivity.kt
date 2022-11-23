2dd91c403a888966ed758971b4dbc0f5e84e5368
VinitK <vinitkr28@gmail.com>
2022-11-24 01:18:23 +0530

    - code refactor

--- module1/src/main/java/vinit/module1/MainActivity.kt
+++ module1/src/main/java/vinit/module1/MainActivity.kt
@@ -3,12 +3,21 @@ package vinit.module1
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import vinit.module1.ui.MainFragment
+import android.util.Log.d
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        d("MainActivity", "BASE_URL_EU_DEVELOPMENT: ${BuildConfig.BASE_URL_EU_DEVELOPMENT}")
+        d("MainActivity", "APPLICATION_ID: ${BuildConfig.APPLICATION_ID}")
+        d("MainActivity", "BUILD_TYPE: ${BuildConfig.BUILD_TYPE}")
+        d("MainActivity", "VERSION_CODE: ${BuildConfig.VERSION_CODE}")
+        d("MainActivity", "VERSION_NAME: ${BuildConfig.VERSION_NAME}")
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
